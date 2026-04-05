58606940a3f60d0ca3fed8c5320e53da1b21435e
aditya maurya <aditya2018m@gmail.com>
2026-04-05 18:50:54 +0530

    updatred

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
-import { connectDb } from "@/lib/db";
 
 export default async function Home() {
   const session = await auth();
-  await connectDb();
   redirect(session ? "/chat" : "/login");
 }
